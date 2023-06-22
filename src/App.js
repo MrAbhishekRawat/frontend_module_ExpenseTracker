015c4fb8ead1a77b45382d0c4f005f4e4cccb63c
MrAbhishekRawat <abhishekr.rawat8@gmail.com>
2023-06-22 12:21:46 +0530

    e.t task1

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import { Routes, Route, useNavigate, Navigate } from "react-router-dom";
+import { useContext, useEffect } from "react";
+import AuthContext from "./store/AuthContext";
+import LoginPage from "./components/LoginPage";
+import Home from "./components/Home";
+
+function App() {
+  const authCtx = useContext(AuthContext);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!authCtx.isLoggedIn) {
+      navigate("/", { replace: true });
+    }
+  }, [authCtx.isLoggedIn, navigate]);
+
+  return (
+   
+      <Routes>
+        <Route path="/" element={<LoginPage />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="*" element={authCtx.isLoggedIn ? null : <Navigate to="/" replace />} />
+      </Routes>
+  );
+}
+
+export default App;
